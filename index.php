ca0bbe8094d0a895c30141e9f90daf03ca562d8e
moheck <mcheck96@gmail.com>
2016-02-02 20:26:42 -0700

    index content

--- index.php
+++ index.php
@@ -5,8 +5,16 @@
 </head>
 
 <body>
-<h1> Hello World</h1>
+<h1>Morgan Heck </h1>
 
-<p> What a beautiful world</p>
+<h2>University of Arizona sophomore</h2>
+<h2>Software Developer</h2>
+<h2>Microsoft Student Partner</h2>
+<h2>Hack Arizona Organizer</h2>
+
+<h3>Links</h3>
+<p><a href="https://www.linkedin.com/in/morgan-heck-04aa3a94?trk=nav_responsive_tab_profile">www.linkedin.com/in/morgan-heck</a></p>
+<p><a href="https://www.instagram.com/mo.heck/">https://www.instagram.com/mo.heck</a></p>
+
 </body>
 </html>
