669b6e3f3f099b889985eaa7050afabe14f941b1
moheck <mcheck96@gmail.com>
2016-02-02 22:13:38 -0700

    more design

--- index.php
+++ index.php
@@ -3,23 +3,46 @@
 <head>
 <title>Morgan Heck</title>
 <style>
+*{
+  margin: 0; padding: 0;
+}
+body{
+	background-color: #484848;
+}
 p{
 font-family:"Impact, Charcoal, sans-serif";
+color:#FFFFFF;
+}
+a{
+	font-family:"Impact, Charcoal, sans-serif";
+	color:#FFFFFF;
 }
 h1{
 font-family:"Impact, Charcoal, sans-serif";
+color:#FFFFFF;
 }
 h5{
 font-family:"Impact, Charcoal, sans-serif";
+color:#FFFFFF;
 }
 h2{
 font-family:"Impact, Charcoal, sans-serif";
+color:#FFFFFF;
 }
 #header{
+	background: -webkit-gradient(linear, left top, right bottom,from(#484848),to(#0066FF));
+}
+#mid{
+	text-align: center;
+	background: white;
 }
 #quote{
+	color:black;
 }
-
+#bot{
+	background-color: #484848;
+}
+#
 </style>
 </head>
 
@@ -34,16 +57,17 @@ font-family:"Impact, Charcoal, sans-serif";
 
 </div>
 
-<div id="quote">
-<p><em>A wise man will make more opportunities than he finds.</em></p>
+<div id="mid">
+<p id="quote"><br><br><em>A wise man will make more opportunities than he finds.</em><br><br><br></p>
  </div>
 
+<div id="bot">
 <h2>Contact</h2>
 <p>mcheck96@gmail.com</p>
 <p>(858) 722-7734</p>
 <h2>Links</h2>
 <p><a href="https://www.linkedin.com/in/morgan-heck-04aa3a94?trk=nav_responsive_tab_profile">www.linkedin.com/in/morgan-heck</a></p>
 <p><a href="https://www.instagram.com/mo.heck/">https://www.instagram.com/mo.heck</a></p>
-
+</div>
 </body>
 </html>
