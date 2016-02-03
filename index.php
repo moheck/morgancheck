667e2a481a3e3fa2007c37780bb547023e124c70
moheck <mcheck96@gmail.com>
2016-02-02 20:46:21 -0700

    early design

--- index.php
+++ index.php
@@ -2,17 +2,46 @@
 <html>
 <head>
 <title>Morgan Heck</title>
+<style>
+p{
+font-family:"Impact, Charcoal, sans-serif";
+}
+h1{
+font-family:"Impact, Charcoal, sans-serif";
+}
+h5{
+font-family:"Impact, Charcoal, sans-serif";
+}
+h2{
+font-family:"Impact, Charcoal, sans-serif";
+}
+#header{
+}
+#quote{
+}
+
+</style>
 </head>
 
 <body>
+<div id="header">
 <h1>Morgan Heck </h1>
 
-<h2>University of Arizona sophomore</h2>
-<h2>Software Developer</h2>
-<h2>Microsoft Student Partner</h2>
-<h2>Hack Arizona Organizer</h2>
+<h5>University of Arizona sophomore</h5>
+<h5>Software Developer</h5>
+<h5>Microsoft Student Partner</h5>
+<h5>Hack Arizona Organizer</h5>
 
-<h3>Links</h3>
+</div>
+
+<div id="quote">
+<p><em>A wise man will make more opportunities than he finds.</em></p>
+ </div>
+
+<h2>Contact</h2>
+<p>mcheck96@gmail.com</p>
+<p>(858) 722-7734</p>
+<h2>Links</h2>
 <p><a href="https://www.linkedin.com/in/morgan-heck-04aa3a94?trk=nav_responsive_tab_profile">www.linkedin.com/in/morgan-heck</a></p>
 <p><a href="https://www.instagram.com/mo.heck/">https://www.instagram.com/mo.heck</a></p>
 
