99e0fa96996ea27eb80e9b2cf65980d967d914bf
moheck <mcheck96@gmail.com>
2016-02-03 17:48:20 -0700

    pic resize/design

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 <html>
 <head>
 <title>Morgan Heck</title>
+<link rel="icon" type="image/ico" href="/Users/morganheck/morgancheck/favicon.ico"/>
 
-<link rel="icon" href="favicon.ico">
 
 <style>
 *{
@@ -11,29 +11,27 @@
 }
 body{
 	background-color: #484848;
+	font-family:Helvetica;
+	text-align: center;
 }
 p{
-font-family:"Impact, Charcoal, sans-serif";
-color:#FFFFFF;
+color:black;
 }
 a{
-	font-family:"Impact, Charcoal, sans-serif";
-	color:#FFFFFF;
+	color:black;
 }
 h1{
-font-family:"Impact, Charcoal, sans-serif";
-color:#FFFFFF;
+color:black;
 }
 h5{
-font-family:"Impact, Charcoal, sans-serif";
-color:#FFFFFF;
+color:black;
 }
 h2{
-font-family:"Impact, Charcoal, sans-serif";
-color:#FFFFFF;
+color:black;
 }
 #header{
 	background: -webkit-gradient(linear, left top, right bottom,from(#484848),to(#0066FF));
+	padding: 64px 0;
 }
 #mid{
 	text-align: center;
@@ -43,20 +41,40 @@ color:#FFFFFF;
 	color:black;
 }
 #bot{
-	background-color: #484848;
+	background-color: #DDD;
 }
-#
+#college{
+	background:#DDD ;
+}
+#hackaz{
+	background:white;
+}
+#dev{
+	background:#DDD;
+}
+#micro{
+	background:white;
+}
+.section{
+	height:200px;
+	padding: 0 64px;
+}
+#header h1, #header h5 a{
+	color:white;
+}
+
+
 </style>
 </head>
 
 <body>
 <div id="header">
 <h1>Morgan Heck </h1>
-<img src="IMG_5251.jpg" height=175>
-<h5>University of Arizona sophomore</h5>
-<h5>Software Developer</h5>
-<h5>Microsoft Student Partner</h5>
-<h5>Hack Arizona Organizer</h5>
+<img src="/Users/morganheck/morgancheck/IMG_5251.jpg" height=175>
+<h5><a href="#college">University of Arizona sophomore</a></h5>
+<h5><a href="#dev">Software Developer</a></h5>
+<h5><a href="#micro">Microsoft Student Partner</a></h5>
+<h5><a href="#hackaz">Hack Arizona Organizer</a></h5>
 
 </div>
 
@@ -64,7 +82,34 @@ color:#FFFFFF;
 <p id="quote"><br><br><em>A wise man will make more opportunities than he finds.</em><br><br><br></p>
  </div>
 
-<div id="bot">
+ <div id="college" class="section">
+ <br><br><br>
+ <h2>University of Arizona sophomore</h2>
+ <p> I am a computer science major at the U of A. Five days a weeek I am on campus, going to classes and doing schoolwork. I also attend weekly meetings with the InnovateUA chapter, an amazing group of students that organize and attend tech events on and off campus. </p>
+ </div>
+
+  <div id="hackaz" class="section">
+   <br><br><br>
+ <h2> Hack Arizona Organizer</h2>
+ <p> Hack Arizona is sweet</p>
+ </div>
+
+ <div id="dev" class="section">
+  <br><br><br>
+ <h2> Software Developer</h2>
+ <p> I am always trying to learn more about software. For example, I built this website myself to experiment with front-end development, and also to learn how to launch my own website. I love being a developer because there is always more to learn. I enjoy working with people, sharing what I know and learning from others. 
+ </div>
+
+ <div id="micro" class="section">
+  <br><br><br>
+ <h2> Microsoft Student Partner</h2>
+ <p> This summer I am interning on the Microsoft Office engineering team. I am also a campus partner with Microsoft. That being said, I am a proud representation of Microsoft. I aim to share Microsoft products and developer tools, and teach others how they work. 
+ </div>
+
+
+
+<div id="bot" class="section">
+ <br><br><br>
 <h2>Contact</h2>
 <p>mcheck96@gmail.com</p>
 <p>(858) 722-7734</p>
