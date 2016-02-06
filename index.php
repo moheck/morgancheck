27ae4a2670fef6af754327d10223f50151774149
moheck <mcheck96@gmail.com>
2016-02-06 15:27:48 -0700

    title change

--- index.php
+++ index.php
@@ -22,6 +22,270 @@ a{
 }
 h1{
 color:black;
+font-family: Verdana, Geneva, sans-serif;
+
+}
+h5{
+color:black;
+}
+h2{
+color:black;
+}
+#header{
+	background: -webkit-gradient(linear, left top, right bottom,from(#484848),to(#0066FF));
+	padding: 64px 0;
+	position: relative;
+}
+#mid{
+	text-align: center;
+	background: white;
+}
+#quote{
+	color:black;
+}
+#bot{
+	background-color: #DDD;
+}
+#college{
+	background:#DDD ;
+}
+#hackaz{
+	background:#DDD;
+}
+#dev{
+	background:white;
+}
+#micro{
+	background:white;
+}
+.section{
+	height:250px;
+	padding: 0 64px;
+	border-top-style: solid;
+}
+#header h1, #header h5 a{
+	color:white;
+}
+h1 span{
+	border-bottom: 4px solid;
+	display:block;
+	margin-top:-40px;
+}
+
+
+</style>
+</head>
+
+<body>
+<div id="header">
+<h1><span>Morgan Heck</span></h1>
+<br>
+<img src="IMG_5251.jpg" height=175>
+<h5><a href="#college">University of Arizona sophomore</a></h5>
+<h5><a href="#dev">Software Developer</a></h5>
+<h5><a href="#micro">Microsoft Student Partner</a></h5>
+<h5><a href="#hackaz">Hack Arizona Organizer</a></h5>
+
+</div>
+
+<div id="mid">
+<p id="quote"><br><br><em>A wise man will make more opportunities than he finds.</em><br><br><br></p>
+ </div>
+
+ <div id="college" class="section">
+ <br><br><br>
+ <h2>University of Arizona sophomore</h2>
+ <p> I am a computer science major at the U of A. I've met lots of great people here and I love what I study. Five days a weeek I am on campus, going to classes and doing schoolwork. I also attend weekly meetings with the InnovateUA chapter, an amazing group of students that organize and attend tech events on and off campus. </p>
+ </div>
+
+  <div id="hackaz" class="section">
+   <br><br><br>
+ <h2> Hack Arizona Organizer</h2>
+ <p> I got on board the Hack Arizona organizing team Fall semester 2015. Helping organize what would be my first hackathon was an interesting experience, but, I loved the idea and I wanted to help.</p><br>
+ <p> It was an amazing event for many reasons. What I really liked was that I was constantly learning from the brilliant people around me. It was an innovation machine that ran for 36 hours, made of talented students, tech industry proffessionals, and various faculty. Ideas and knowledge flowed like moving parts throughout the library, and imaginations were brought to life. I was happy to be a part of it.</p>
+ </div>
+
+ <div id="dev" class="section">
+  <br><br><br>
+ <h2> Software Developer</h2>
+ <p> I am always trying to learn more about software. For example, I built this website myself to experiment with front-end development, and also to learn how to launch my own website. I love being a developer because there is always more to learn. I enjoy working with people, sharing what I know and learning from others. 
+ </div>
+
+ <div id="micro" class="section">
+  <br><br><br>
+ <h2> Microsoft Student Partner</h2>
+ <p> This summer I am interning on the Microsoft Office engineering team. I am also a campus partner with Microsoft. That being said, I am a proud representation of Microsoft. I aim to share Microsoft products and developer tools, and teach others how they work. 
+ </div>
+
+
+
+<div id="bot" class="section">
+ <br><br><br>
+<h2>Contact</h2>
+<p>mcheck96@gmail.com</p>
+<p>(858) 722-7734</p>
+<h2>Links</h2>
+<p><a href="https://www.linkedin.com/in/morgan-heck-04aa3a94?trk=nav_responsive_tab_profile">www.linkedin.com/in/morgan-heck</a></p>
+<p><a href="https://www.instagram.com/mo.heck/">https://www.instagram.com/mo.heck</a></p>
+</div>
+</body>
+</html>
+<!DOCTYPE html>
+<html>
+<head>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Morgan Heck</title>
+<link rel="icon" type="image/ico" href="/Users/morganheck/morgancheck/favicon.ico"/>
+
+
+<style>
+*{
+  margin: 0; padding: 0;
+}
+body{
+	background-color: #484848;
+	font-family:Helvetica;
+	text-align: center;
+}
+p{
+color:black;
+}
+a{
+	color:black;
+}
+h1{
+color:black;
+font-family: Verdana, Geneva, sans-serif;
+
+}
+h5{
+color:black;
+}
+h2{
+color:black;
+}
+#header{
+	background: -webkit-gradient(linear, left top, right bottom,from(#484848),to(#0066FF));
+	padding: 64px 0;
+	position: relative;
+}
+#mid{
+	text-align: center;
+	background: white;
+}
+#quote{
+	color:black;
+}
+#bot{
+	background-color: #DDD;
+}
+#college{
+	background:#DDD ;
+}
+#hackaz{
+	background:#DDD;
+}
+#dev{
+	background:white;
+}
+#micro{
+	background:white;
+}
+.section{
+	height:250px;
+	padding: 0 64px;
+	border-top-style: solid;
+}
+#header h1, #header h5 a{
+	color:white;
+}
+h1 span{
+	border-bottom: 4px solid;
+	display:block;
+	margin-top:-40px;
+}
+
+
+</style>
+</head>
+
+<body>
+<div id="header">
+<h1><span>Morgan Heck</span></h1>
+<br>
+<img src="IMG_5251.jpg" height=175>
+<h5><a href="#college">University of Arizona sophomore</a></h5>
+<h5><a href="#dev">Software Developer</a></h5>
+<h5><a href="#micro">Microsoft Student Partner</a></h5>
+<h5><a href="#hackaz">Hack Arizona Organizer</a></h5>
+
+</div>
+
+<div id="mid">
+<p id="quote"><br><br><em>A wise man will make more opportunities than he finds.</em><br><br><br></p>
+ </div>
+
+ <div id="college" class="section">
+ <br><br><br>
+ <h2>University of Arizona sophomore</h2>
+ <p> I am a computer science major at the U of A. I've met lots of great people here and I love what I study. Five days a weeek I am on campus, going to classes and doing schoolwork. I also attend weekly meetings with the InnovateUA chapter, an amazing group of students that organize and attend tech events on and off campus. </p>
+ </div>
+
+  <div id="hackaz" class="section">
+   <br><br><br>
+ <h2> Hack Arizona Organizer</h2>
+ <p> I got on board the Hack Arizona organizing team Fall semester 2015. Helping organize what would be my first hackathon was an interesting experience, but, I loved the idea and I wanted to help.</p><br>
+ <p> It was an amazing event for many reasons. What I really liked was that I was constantly learning from the brilliant people around me. It was an innovation machine that ran for 36 hours, made of talented students, tech industry proffessionals, and various faculty. Ideas and knowledge flowed like moving parts throughout the library, and imaginations were brought to life. I was happy to be a part of it.</p>
+ </div>
+
+ <div id="dev" class="section">
+  <br><br><br>
+ <h2> Software Developer</h2>
+ <p> I am always trying to learn more about software. For example, I built this website myself to experiment with front-end development, and also to learn how to launch my own website. I love being a developer because there is always more to learn. I enjoy working with people, sharing what I know and learning from others. 
+ </div>
+
+ <div id="micro" class="section">
+  <br><br><br>
+ <h2> Microsoft Student Partner</h2>
+ <p> This summer I am interning on the Microsoft Office engineering team. I am also a campus partner with Microsoft. That being said, I am a proud representation of Microsoft. I aim to share Microsoft products and developer tools, and teach others how they work. 
+ </div>
+
+
+
+<div id="bot" class="section">
+ <br><br><br>
+<h2>Contact</h2>
+<p>mcheck96@gmail.com</p>
+<p>(858) 722-7734</p>
+<h2>Links</h2>
+<p><a href="https://www.linkedin.com/in/morgan-heck-04aa3a94?trk=nav_responsive_tab_profile">www.linkedin.com/in/morgan-heck</a></p>
+<p><a href="https://www.instagram.com/mo.heck/">https://www.instagram.com/mo.heck</a></p>
+</div>
+</body>
+</html>
+<title>Morgan Heck</title>
+<link rel="icon" type="image/ico" href="/Users/morganheck/morgancheck/favicon.ico"/>
+
+
+<style>
+*{
+  margin: 0; padding: 0;
+}
+body{
+	background-color: #484848;
+	font-family:Helvetica;
+	text-align: center;
+}
+p{
+color:black;
+}
+a{
+	color:black;
+}
+h1{
+color:black;
 font-family:Verdana, Geneva, sans-serif;
 }
 h5{
